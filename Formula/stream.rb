f1529b53633a36f0a79c8c11b10d492c49e3229a
Kaitian Xie <kaitian96@outlook.com>
2022-03-27 09:01:14 +0800

    add stream.rb

---
+++ Formula/stream.rb
@@ -0,0 +1,30 @@
+class Stream < Formula
+  desc "DevStream: the   open-source DevOps toolchain manager (DTM)."
+  homepage ""
+  url "https://github.com/merico-dev/stream/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "3223ff4ea46626d6849831ad9abfa6f67699731fd927fe477ab945b3ab9424eb"
+  license "Apache-2.0"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test stream`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
